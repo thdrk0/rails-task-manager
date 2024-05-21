50184aa3306aa9c22344db73995d3584baf6d41f
theodorik comlan <comlantheodorik@gmail.com>
2024-05-21 15:25:41 +0200

    task done

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,14 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  root to: "tasks#index"
   # root "posts#index"
+  # get "tasks", to: "tasks#index"
+  # get "tasks/new", to: "tasks#new"
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
 end
